86dade054b4d3bbb8c29aad41e6e0e6e1e89e844
Arnaud Etitia <etitiaa@desktop-278.gipsa-lab.grenoble-inp.fr>
2014-04-11 17:06:22 +0200

    nouvelle extractioon de contour

--- volumetric/impreciseBoundary.cpp
+++ volumetric/impreciseBoundary.cpp
@@ -8,14 +8,21 @@
 
 #include "DGtal/base/Common.h"
 #include "DGtal/helpers/StdDefs.h"
+
 #include "DGtal/kernel/BasicPointPredicates.h"
 #include "DGtal/kernel/sets/SetPredicate.h"
 
 #include "DGtal/topology/KhalimskySpaceND.h"
+#include "DGtal/topology/MetricAdjacency.h"
+#include "DGtal/topology/DomainMetricAdjacency.h"
+#include "DGtal/topology/DomainAdjacency.h"
+#include "DGtal/topology/DigitalTopology.h"
+#include "DGtal/topology/Object.h"
+
+#include "DGtal/topology/helpers/Surfaces.h"
 
 #include "DGtal/shapes/ShapeFactory.h"
 #include "DGtal/shapes/Shapes.h"
-#include "DGtal/kernel/sets/SetPredicate.h"
 
 #include "DGtal/images/imagesSetsUtils/SimpleThresholdForegroundPredicate.h"
 #include "DGtal/images/ImageSelector.h"
@@ -34,12 +41,14 @@
 #include "DGtal/io/colormaps/HueShadeColorMap.h"
 #include "DGtal/io/boards/Board2D.h"
 
-#include "DGtal/topology/helpers/Surfaces.h"
-
+#include "DGtal/graph/Expander.h"
 
 
 using namespace std;
 using namespace DGtal;
+typedef SpaceND<2> Z2;
+typedef HyperRectDomain< Z2 > Domain; 
+typedef DigitalSetSelector < Domain, BIG_DS + HIGH_BEL_DS >::Type DigitalSet;
 typedef GrayscaleColorMap<unsigned char> Gray;
 typedef ImageSelector<Z2i::Domain, unsigned int>::Type Image;
 typedef IntervalThresholder<Image::Value> Binarizer; 
@@ -50,6 +59,13 @@ typedef HueShadeColorMap<long int, 2> HueTwice;
 typedef NotPointPredicate<Z2i::DigitalSet> NotPredicate;
 typedef ExactPredicateLpSeparableMetric<Z2i::Space, 2> L2Metric;
 typedef VoronoiMap<Z2i::Space, NotPredicate, L2Metric > Voronoi2D;
+typedef MetricAdjacency<Z2, 1> Adj4;
+typedef MetricAdjacency<Z2, 2> Adj8;
+typedef DigitalTopology< Adj8, Adj4 > DT8_4;
+typedef DigitalTopology< Adj4, Adj8 > DT4_8;
+typedef Object<DT4_8, DigitalSet> ObjectType;
+typedef Object<DT4_8, DigitalSet> ObjectType48;
+typedef typename DigitalSet::ConstIterator DigitalSetConstIterator;
 
 void readData(char *filename,vector<int>&x ,vector<int>&y ,vector<int>&noiseLevel,vector<int>&freeman){
 	ifstream data(filename);
@@ -76,16 +92,120 @@ void readData(char *filename,vector<int>&x ,vector<int>&y ,vector<int>&noiseLeve
 	data.close();
 } 
 
-void getContours(vector< vector< Z2i::Point >  >&  contours,Image &image){
-	
-	Binarizer b(100,200);
-	PointFunctorPredicate<Image,Binarizer> predicate(image, b);
+Z2i::Point regard(int i){
+	switch(i){
+		case 0: return (Z2i::Point(1,0));
+		case 1: return (Z2i::Point(0,1)); 
+		case 2: return (Z2i::Point(-1,0)); 
+		case 3: return (Z2i::Point(0,-1));
+	}
+}
 
-	Z2i::KSpace ks;
-	if (!ks.init( image.domain().lowerBound(), image.domain().upperBound(), true )) cout << "pb" << endl;
-        
-	SurfelAdjacency<2> sAdj( true );
-	Surfaces<Z2i::KSpace>::extractAllPointContours4C( contours,ks, predicate, sAdj ); 
+void getContours(vector< vector< Z2i::Point >  >&  contours,Image &image,vector<int> & x,vector<int> & y,vector<int> &noise){
+	
+	DigitalSet image_set ( image.domain() );
+	int maxX = image.domain().upperBound()[0];
+	int maxY = image.domain().upperBound()[1];
+	for (unsigned int i = 0;i<maxX; i++){
+		for (unsigned int j = 0;j<maxY; j++){
+			Z2i::Point p(i,j);
+			if (image(p) == 0) image_set.insertNew(p); 
+    		}
+	}
+	Adj4 adj4;
+	Adj8 adj8;
+	DT4_8 dt4_8 ( adj4, adj8, JORDAN_DT );
+	ObjectType im ( dt4_8, image_set );
+	ObjectType imageBorder = im.border();
+	Image tmp(image.domain());
+	Board2D board;
+	for ( Image::Iterator it = tmp.begin(), itend = tmp.end();it != itend; ++it)
+    		(*it)=128;
+	DigitalSetConstIterator it_end = imageBorder.end();
+	DigitalSetConstIterator it_begin = imageBorder.begin(); 
+	int currentX,currentY;
+	vector<Z2i::Point> cont1;
+	vector<Z2i::Point> cont2;
+	for ( DigitalSetConstIterator it = it_begin;it != it_end;++it ) {
+		tmp.setValue((*it),0);	
+	}
+	Z2i::Point depart = Z2i::Point((*it_begin)[0],(*it_begin)[1]);
+	Z2i::Point currentPT = depart;
+	int direction = 0;
+	do{
+		for (int i=0;i<4;i++){ 
+			if (tmp(currentPT+regard((direction+i)%4)) == 0) {
+				currentPT += regard((direction+i)%4);
+				cont1.push_back(currentPT); 
+				direction = (direction +i+3)%4;
+				break;
+			}  
+		} 
+	}while (currentPT[0] != depart[0] || currentPT[1] != depart[1]);
+	cont1.push_back(currentPT);
+	int t;
+	int nbVoisin;
+	for ( DigitalSetConstIterator it = it_begin;it != it_end;++it ) {
+		currentX = (*it)[0];
+		currentY = (*it)[1];
+		for (int i = 0; i<x.size();i++){
+			if ((x[i] == currentX) && (y[i] == maxY-currentY)){
+				if (noise[i] == 1){
+					Z2i::Point pt = Z2i::Point((*it)); 
+					if (tmp(pt) == 0){
+						nbVoisin = 0;
+						if (tmp(pt+regard(0)) == 0) nbVoisin++;
+						if (tmp(pt+regard(1)) == 0) nbVoisin++;
+						if (tmp(pt+regard(2)) == 0) nbVoisin++;
+						if (tmp(pt+regard(3)) == 0) nbVoisin++;						
+					} 
+				} 
+			}
+			if  (nbVoisin == 2) {depart= (*it);break;}
+		}   	
+	} 
+	currentPT = depart;
+	direction = 0;
+	do{
+		for (int i=0;i<3;i++){ 
+			if (tmp(currentPT+regard((direction+i)%4)) == 0) {
+				t = i;
+			}	 
+		} 
+		currentPT += regard((direction+t)%4);
+		cont2.push_back(currentPT); 
+		direction = (direction+t+3)%4;
+	}while (currentPT[0] != depart[0] || currentPT[1] != depart[1]);
+	cont2.push_back(currentPT); 
+	/*int trouve1,trouve2; 
+	for ( DigitalSetConstIterator it = it_begin;it != it_end;++it ) {
+		currentX = (*it)[0];
+		currentY = (*it)[1];
+		trouve1 = 0;
+		for (int i = 0;i<x.size();i++){
+			if ((x[i] == currentX) && (y[i] == maxY-currentY)) {
+				cont2.push_back((*it));
+				trouve1 =1;
+				break; 
+			}
+		}
+		if (!trouve1){
+			trouve2 = 0;
+			for (int i = 0;i<cont1.size();i++){
+				if ( (cont1[i][0] == currentX)  && (cont1[i][1] == maxYcurrentY) ) {
+					trouve2 = 1;
+					break;
+				}
+			}
+			if (!trouve2) cont2.push_back((*it));	
+		} 
+	}*/
+	contours.push_back(cont1);
+	contours.push_back(cont2);
+	Display2DFactory::drawImage<Gray>(board, tmp, (unsigned int)0, (unsigned int)129);
+	
+	board.saveSVG ( "../../../imageBorder.svg" );
+	
 } 
 
 void constructImage(vector< Z2i::Point >& contour,Image& image){
@@ -107,18 +227,72 @@ void contourBallReduced(vector<int>& x,vector<int>& y,vector<int>& noiseLevel,ve
     		(*it)=128;
 	int cpt = 0;
 	for (int i =1;i<x.size()-1;i++){
-		if ( (noiseLevel[i] >= 2) &&  
-		     (noiseLevel[i] > noiseLevel[i-1]) && 
-		     (noiseLevel[i] > noiseLevel[i+1])) cpt ++;
+		if (
+		   	( 
+			  (noiseLevel[i] >= 2) &&  
+		     	  (noiseLevel[i] > noiseLevel[i-1]) && 
+		     	  (noiseLevel[i] > noiseLevel[i+1]) 
+		   	)
+		   || 
+		   	(
+		     		(
+					(
+						(noiseLevel[i] == noiseLevel[i-1]) && 
+		       				(noiseLevel[i] - noiseLevel[i+1] > 2 )
+					) 
+		    			|| 
+					(
+						(noiseLevel[i] == noiseLevel[i+1]) && 
+		       				(noiseLevel[i] - noiseLevel[i-1] > 2 )
+					) 
+		   		)
+			)
+		   ) cpt ++;
 	}
 
-	if ((noiseLevel[0] >= 2) && 
-	(noiseLevel[0] > noiseLevel[x.size()-1]) && 
-	(noiseLevel[0] > noiseLevel[1])) cpt ++;
+	if (
+		   ( 
+			 (noiseLevel[0] >= 2) &&  
+		     	 (noiseLevel[0] > noiseLevel[x.size()-1]) && 
+		     	 (noiseLevel[0] > noiseLevel[1]) 
+		   )
+		   || 
+		   (
+		     	(
+				(
+					(noiseLevel[0] == noiseLevel[x.size()-1]) && 
+		       			(noiseLevel[0] - noiseLevel[1] > 2 )
+				) 
+		    		|| 
+				(
+					(noiseLevel[0] == noiseLevel[1]) && 
+		       			(noiseLevel[0] - noiseLevel[x.size()-1] > 2 )
+				) 
+		   	)
+		)
+	    ) cpt ++;
 	
-	if ((noiseLevel[x.size()-1] >= 2) &&
-	(noiseLevel[x.size()-1] > noiseLevel[x.size()-2]) && 
-	(noiseLevel[x.size()-1] > noiseLevel[0])) cpt ++;
+	if (
+		   ( 
+			 (noiseLevel[x.size()-1] >= 2) &&  
+		     	 (noiseLevel[x.size()-1] > noiseLevel[x.size()-2]) && 
+		     	 (noiseLevel[x.size()-1] > noiseLevel[0]) 
+		   )
+		   || 
+		   (
+		     	(
+				(
+					(noiseLevel[x.size()-1] == noiseLevel[x.size()-2]) && 
+		       			(noiseLevel[x.size()-1] - noiseLevel[0] > 2 )
+				) 
+		    		|| 
+				(
+					(noiseLevel[x.size()-1] == noiseLevel[0]) && 
+		       			(noiseLevel[x.size()-1] - noiseLevel[x.size()-2] > 2 )
+				) 
+		   	)
+		)
+	    ) cpt ++;
 
 	cout << "Nombre de cas interessant : " << cpt << " sur " << x.size() << endl; 
 	cout << "Soit un Ratio de "<< 100 * cpt / x.size() << "%" <<  endl;
@@ -325,7 +499,7 @@ void computeDTDiff(Image & image1,Image & image2,int maxX,int maxY,char* outputf
 
 int main(int argc,char **argv){
 	if (argc != 2){
-		cout << "Usage : ./impreciseBounadary <filename> " << endl;
+		cout << "Usage : ./impreciseBounadary <filename>" << endl;
 		exit(1);
 	}	 
 	char *filename = argv[1];
@@ -334,10 +508,11 @@ int main(int argc,char **argv){
 	vector<int>noiseLevel;
 	vector<int>freeman;
 	readData(filename,x,y,noiseLevel,freeman);// récupération des données de contours 
-	x.pop_back();
-	y.pop_back();
-	noiseLevel.pop_back();
-	freeman.pop_back();
+	//x.pop_back();
+	//y.pop_back();
+	//noiseLevel.pop_back();
+	//freeman.pop_back();
+	
 
 	//boite englobante
 	int valMaxX = x[0];
@@ -363,9 +538,10 @@ int main(int argc,char **argv){
     		(*it)=128;
 
 	for (int i=0;i<x.size();i++){
-		for (int a = max(0,x[i]-(noiseLevel[i]-1) ) ; a <= min(x[i]+(noiseLevel[i]-1),valMaxX); a++ ){
-			for (int b = max(0,y[i]-(noiseLevel[i]-1) ) ; b <= min(y[i]+(noiseLevel[i]-1),valMaxY); b++ ){
+		for (int a = max(0,x[i]-(noiseLevel[i]-1) ) ; a <= min(x[i]+(noiseLevel[i]-1),valMaxX+20); a++ ){
+			for (int b = max(0,y[i]-(noiseLevel[i]-1) ) ; b <= min(y[i]+(noiseLevel[i]-1),valMaxY+20); b++ ){
 				image.setValue( Z2i::Point(a,valMaxY-b+20),0);
+				//image.setValue( Z2i::Point(x[i],valMaxY-y[i]+20),0);
 			}
 		}
 	}
@@ -380,7 +556,7 @@ int main(int argc,char **argv){
 	
 	vector< vector< Z2i::Point >  >  contours;
 	
-	getContours(contours,image);
+	getContours(contours,image,x,y,noiseLevel);
 	Image image1 ( Z2i::Domain(lower,upper));
 	Image image2 ( Z2i::Domain(lower,upper));
 	Image imageCB ( Z2i::Domain(lower,upper));
@@ -402,7 +578,7 @@ int main(int argc,char **argv){
 
 	//Calcul de la transformée en distance 
 	
-	computeDT(image1,"../../../DT_contour1.svg"); 
+	/*computeDT(image1,"../../../DT_contour1.svg"); 
 	computeDT(image2,"../../../DT_contour2.svg"); 
 	computeDT(imageCB,"../../../DT_contourBoth.svg"); 
 
@@ -419,7 +595,7 @@ int main(int argc,char **argv){
 	computeDTDiff(DT1,DT2,valMaxX+20,valMaxY+20,"../../../DT_contourDiff.svg");
 
 	//Création d'un contour avec boules réduites
-	contourBallReduced(x,y,noiseLevel,freeman,valMaxX+20,valMaxY+20); 
+	contourBallReduced(x,y,noiseLevel,freeman,valMaxX+20,valMaxY+20); */
 	
 	return 0; 
 }
